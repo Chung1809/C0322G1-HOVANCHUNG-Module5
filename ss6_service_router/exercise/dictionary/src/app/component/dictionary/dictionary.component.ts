2a2c3422d8dd20ca3c329206b64d90009c6503d8
Chung1809 <supperman1809@gmail.com>
2022-08-20 11:56:44 +0700

    update

---
+++ ss6_service_router/exercise/dictionary/src/app/component/dictionary/dictionary.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Dictionary} from "../../model/dictionary";
+import {DictionaryService} from "../../service/dictionary.service";
+
+@Component({
+  selector: 'app-dictionary',
+  templateUrl: './dictionary.component.html',
+  styleUrls: ['./dictionary.component.css']
+})
+export class DictionaryComponent implements OnInit {
+
+  dictionaries: Dictionary[];
+  searchWord: string;
+
+  constructor(private dictionaryService: DictionaryService) {
+  }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this.dictionaries = this.dictionaryService.getAll();
+    console.log(this.dictionaries);
+  }
+
+  searchByWord() {
+    this.dictionaries = this.dictionaryService.searchByWord(this.searchWord);
+  }
+
+}
